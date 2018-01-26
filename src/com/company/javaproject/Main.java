dc8b1c5b85888d2dd9d5d4f96f7b2a5297612fb7
Jonathan Baricot <jbaricot@gmail.com>
2018-01-25 20:49:33 -0500

    Initial commit

---
+++ src/com/company/javaproject/Main.java
@@ -0,0 +1,10 @@
+package com.company.javaproject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.printl("Hola Mundo");
+    }
+}
